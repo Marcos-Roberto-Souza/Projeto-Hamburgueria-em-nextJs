ae7171c8a503dfc1045f8bce6116c972ca8b3737
Marcos Roberto <277156692+Marcos-Roberto-Souza@users.noreply.github.com>
2026-08-10 19:48:11 -0300

    colocando o http no link para sincronizar com o backend

--- hamburgueria-frontend/src/services/api.ts
+++ hamburgueria-frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'projeto-hamburgueria-em-nextjs-production.up.railway.app',
+    baseURL: "https://projeto-hamburgueria-em-nextjs-production.up.railway.app",
 });
