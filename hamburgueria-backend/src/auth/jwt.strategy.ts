3f2a24944c5b4f3d30f03a5a7c6a92c3aac0d333
Marcos Roberto <277156692+Marcos-Roberto-Souza@users.noreply.github.com>
2026-08-15 01:26:11 -0300

    colocando console para ver se está funcionando jwtstrategy

--- hamburgueria-backend/src/auth/jwt.strategy.ts
+++ hamburgueria-backend/src/auth/jwt.strategy.ts
@@ -6,6 +6,9 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
+    console.log('JWT STRATEGY CARREGADA');
+    console.log('JWT SECRET: ', process.env.JWT_SECRET);
+    
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 
